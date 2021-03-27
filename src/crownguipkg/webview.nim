b90969e5db37451c024fc56efc74d9c1441d50e0
bung87 <crc32@qq.com>
2021-03-27 22:17:48 +0800

    format

--- src/crownguipkg/webview.nim
+++ src/crownguipkg/webview.nim
@@ -1,25 +1,27 @@
-import tables, strutils, macros, json, os, base64,strformat,std/exitprocs
+import tables, strutils, macros, json, os, base64, strformat, std/exitprocs
 
 const headerC = currentSourcePath().substr(0, high(currentSourcePath()) - 11) & "webview.h"
 {.passc: "-DWEBVIEW_STATIC -DWEBVIEW_IMPLEMENTATION -I" & headerC.}
 when defined(linux):
-  {.passc: "-DWEBVIEW_GTK=1 " & staticExec"pkg-config --cflags gtk+-3.0 webkit2gtk-4.0", passl: staticExec"pkg-config --libs gtk+-3.0 webkit2gtk-4.0".}
+  {.passc: "-DWEBVIEW_GTK=1 " & staticExec"pkg-config --cflags gtk+-3.0 webkit2gtk-4.0",
+      passl: staticExec"pkg-config --libs gtk+-3.0 webkit2gtk-4.0".}
 elif defined(windows):
   {.passc: "-DWEBVIEW_WINAPI=1", passl: "-lole32 -lcomctl32 -loleaut32 -luuid -lgdi32".}
 elif defined(macosx):
-  {.passc: "-DOBJC_OLD_DISPATCH_PROTOTYPES=1 -DWEBVIEW_COCOA=1 -x objective-c", passl: "-framework Cocoa -framework WebKit".}
+  {.passc: "-DOBJC_OLD_DISPATCH_PROTOTYPES=1 -DWEBVIEW_COCOA=1 -x objective-c",
+      passl: "-framework Cocoa -framework WebKit".}
 
 type
-  ExternalInvokeCb* = proc (w: Webview; arg: string)  ## External CallBack Proc 
+  ExternalInvokeCb* = proc (w: Webview; arg: string) ## External CallBack Proc
   WebviewPrivObj {.importc: "struct webview_priv", header: headerC, bycopy.} = object
   WebviewObj* {.importc: "struct webview", header: headerC, bycopy.} = object ## WebView Type
-    url* {.importc: "url".}: cstring                    ## Current URL
-    title* {.importc: "title".}: cstring                ## Window Title
-    width* {.importc: "width".}: cint                   ## Window Width
-    height* {.importc: "height".}: cint                 ## Window Height
-    resizable* {.importc: "resizable".}: cint           ## `true` to Resize the Window, `false` for Fixed size Window
-    debug* {.importc: "debug".}: cint                   ## Debug is `true` when not build for Release
-    invokeCb {.importc: "external_invoke_cb".}: pointer ## Callback proc js:window.external.invoke
+    url* {.importc: "url".}: cstring                                          ## Current URL
+    title* {.importc: "title".}: cstring                                      ## Window Title
+    width* {.importc: "width".}: cint                                         ## Window Width
+    height* {.importc: "height".}: cint                                       ## Window Height
+    resizable* {.importc: "resizable".}: cint ## `true` to Resize the Window, `false` for Fixed size Window
+    debug* {.importc: "debug".}: cint                                         ## Debug is `true` when not build for Release
+    invokeCb {.importc: "external_invoke_cb".}: pointer                       ## Callback proc js:window.external.invoke
     priv {.importc: "priv".}: WebviewPrivObj
     userdata {.importc: "userdata".}: pointer
   Webview* = ptr WebviewObj
@@ -30,8 +32,9 @@ type
   MethodInfo = object
     scope, name, args: string
 const
-  dataUriHtmlHeader* = proc (s:string):string = "data:text/html;charset=utf-8;base64," & base64.encode s  ## Data URI for HTML UTF-8 header string
-  fileLocalHeader* = "file:///"  ## Use Local File as URL
+  dataUriHtmlHeader* = proc (s: string): string = "data:text/html;charset=utf-8;base64," &
+      base64.encode s           ## Data URI for HTML UTF-8 header string
+  fileLocalHeader* = "file:///" ## Use Local File as URL
   jsTemplate = """
     if (typeof $2 === 'undefined') {
       $2 = {};
@@ -70,36 +73,44 @@ const
   """.strip.unindent
 
 var
-  eps = newTable[Webview, TableRef[string, TableRef[string, CallHook]]]() # for bindProc
-  cbs = newTable[Webview, ExternalInvokeCb]() # easy callbacks
-  dispatchTable = newTable[int, DispatchFn]() # for dispatch
+  eps = newTable[Webview, TableRef[string, TableRef[string, CallHook]]]()       # for bindProc
+  cbs = newTable[Webview, ExternalInvokeCb]()                                   # easy callbacks
+  dispatchTable = newTable[int, DispatchFn]()                                   # for dispatch
 
 func init(w: Webview): cint {.importc: "webview_init", header: headerC.}
 func loop(w: Webview; blocking: cint): cint {.importc: "webview_loop", header: headerC.}
-func js*(w: Webview; javascript: cstring): cint {.importc: "webview_eval", header: headerC, discardable.} ## Evaluate a JavaScript cstring, runs the javascript string on the window
-func css*(w: Webview; css: cstring): cint {.importc: "webview_inject_css", header: headerC, discardable.} ## Set a CSS cstring, inject the CSS on the Window
+func js*(w: Webview; javascript: cstring): cint {.importc: "webview_eval", header: headerC,
+    discardable.} ## Evaluate a JavaScript cstring, runs the javascript string on the window
+func css*(w: Webview; css: cstring): cint {.importc: "webview_inject_css", header: headerC,
+    discardable.} ## Set a CSS cstring, inject the CSS on the Window
 func setTitle*(w: Webview; title: cstring) {.importc: "webview_set_title", header: headerC.} ## Set Title of window
-func setColor*(w: Webview; red, green, blue, alpha: uint8) {.importc: "webview_set_color", header: headerC.} ## Set background color of the Window
-func setFullscreen*(w: Webview; fullscreen: bool) {.importc: "webview_set_fullscreen", header: headerC.}     ## Set fullscreen
-func dialog(w: Webview; dlgtype: DialogType; flags: cint; title: cstring; arg: cstring; result: cstring; resultsz: system.csize_t) {.importc: "webview_dialog", header: headerC.}
-func dispatch(w: Webview; fn: pointer; arg: pointer) {.importc: "webview_dispatch", header: headerC.} # dispatch nim func,function will be executed on the UI thread
+func setColor*(w: Webview; red, green, blue, alpha: uint8) {.importc: "webview_set_color",
+    header: headerC.} ## Set background color of the Window
+func setFullscreen*(w: Webview; fullscreen: bool) {.importc: "webview_set_fullscreen", header: headerC.} ## Set fullscreen
+func dialog(w: Webview; dlgtype: DialogType; flags: cint; title: cstring; arg: cstring; result: cstring;
+    resultsz: system.csize_t) {.importc: "webview_dialog", header: headerC.}
+func dispatch(w: Webview; fn: pointer; arg: pointer) {.importc: "webview_dispatch",
+    header: headerC.} # dispatch nim func,function will be executed on the UI thread
 func webview_terminate(w: Webview) {.importc: "webview_terminate", header: headerC.}
 func webview_exit(w: Webview) {.importc: "webview_exit", header: headerC.}
-func jsDebug*(format: cstring) {.varargs, importc: "webview_debug", header: headerC.}  ##  `console.debug()` directly inside the JavaScript context.
+func jsDebug*(format: cstring) {.varargs, importc: "webview_debug",
+    header: headerC.} ##  `console.debug()` directly inside the JavaScript context.
 func jsLog*(s: cstring) {.importc: "webview_print_log", header: headerC.} ## `console.log()` directly inside the JavaScript context.
-func webview(title: cstring; url: cstring; w: cint; h: cint; resizable: cint): cint {.importc: "webview", header: headerC, used.}
+func webview(title: cstring; url: cstring; w: cint; h: cint; resizable: cint): cint {.importc: "webview",
+    header: headerC, used.}
 func setUrl*(w: Webview; url: cstring) {.importc: "webview_launch_external_URL", header: headerC.} ## Set the current URL
-func setIconify*(w: Webview; mustBeIconified: bool) {.importc: "webview_set_iconify", header: headerC.}  ## Set window to be Minimized Iconified
+func setIconify*(w: Webview; mustBeIconified: bool) {.importc: "webview_set_iconify",
+    header: headerC.} ## Set window to be Minimized Iconified
 
-func setBorderless*(w: Webview, decorated: bool) {.inline.} =
+func setBorderless*(w: Webview; decorated: bool) {.inline.} =
   ## Use a window without borders, no close nor minimize buttons.
   when defined(linux): {.emit: "gtk_window_set_decorated(GTK_WINDOW(`w`->priv.window), `decorated`);".}
 
-func setSkipTaskbar*(w: Webview, hint: bool) {.inline.} =
+func setSkipTaskbar*(w: Webview; hint: bool) {.inline.} =
   ## Do not show the window on the Taskbar
   when defined(linux): {.emit: "gtk_window_set_skip_taskbar_hint(GTK_WINDOW(`w`->priv.window), `hint`); gtk_window_set_skip_pager_hint(GTK_WINDOW(`w`->priv.window), `hint`);".}
 
-func setSize*(w: Webview, width: Positive, height: Positive) {.inline.} =
+func setSize*(w: Webview; width: Positive; height: Positive) {.inline.} =
   ## Resize the window to given size
   when defined(linux): {.emit: "gtk_widget_set_size_request(GTK_WINDOW(`w`->priv.window), `width`, `height`);".}
 
@@ -107,16 +118,16 @@ func setFocus*(w: Webview) {.inline.} =
   ## Force focus on the window
   when defined(linux): {.emit: "gtk_widget_grab_focus(GTK_WINDOW(`w`->priv.window));".}
 
-func setOnTop*(w: Webview, mustBeOnTop: bool) {.inline.} =
+func setOnTop*(w: Webview; mustBeOnTop: bool) {.inline.} =
   ## Force window to be on top of all other windows
   when defined(linux): {.emit: "gtk_window_set_keep_above(GTK_WINDOW(`w`->priv.window), `mustBeOnTop`);".}
 
-func setClipboard*(w: Webview, text: cstring) {.inline.} =
+func setClipboard*(w: Webview; text: cstring) {.inline.} =
   ## Set a text cstring on the Clipboard, text must not be empty string
   assert text.len > 0, "text for clipboard must not be empty string"
   when defined(linux): {.emit: "gtk_clipboard_set_text(gtk_clipboard_get(GDK_SELECTION_CLIPBOARD), `text`, -1);".}
 
-func setTrayIcon*(w: Webview, path, tooltip: cstring, visible = true) {.inline.} =
+func setTrayIcon*(w: Webview; path, tooltip: cstring; visible = true) {.inline.} =
   ## Set a TrayIcon on the corner of the desktop. `path` is full path to a PNG image icon. Only shows an icon.
   assert path.len > 0, "icon path must not be empty string"
   when defined(linux): {.emit: """
@@ -167,8 +178,8 @@ template dialogOpenDir*(w: Webview; title = ""): string =
   w.dialog(dtOpen, 1.cint, title, "")
 
 proc generalExternalInvokeCallback(w: Webview; arg: cstring) {.exportc.} =
-    # assign to webview.invokeCb using eps,cbs store user defined proc
-  var handled = false
+  # assign to webview.invokeCb using eps,cbs store user defined proc
+var handled = false
   if eps.hasKey(w):
     try:
       var mi = parseJson($arg).to(MethodInfo)
@@ -293,7 +304,7 @@ func run*(w: Webview) {.inline.} =
   ## `run` starts the main UI loop until the user closes the window or `exit()` is called.
   while w.loop(1) == 0: discard
 
-proc run*(w: Webview, quitProc: proc () {.noconv.}, controlCProc: proc () {.noconv.}, autoClose: static[bool] = true) {.inline.} =
+proc run*(w: Webview; quitProc: proc () {.noconv.}; controlCProc: proc () {.noconv.}; autoClose: static[bool] = true) {.inline.} =
   ## `run` starts the main UI loop until the user closes the window. Same as `run` but with extras.
   ## * `quitProc` is a function to run at exit, needs `{.noconv.}` pragma.
   ## * `controlCProc` is a function to run at CTRL+C, needs `{.noconv.}` pragma.
@@ -310,7 +321,8 @@ func exit*(w: Webview) {.inline.} =
   w.webview_terminate()
   w.webview_exit()
 
-proc webView(title = ""; url = ""; width: Positive = 1000; height: Positive = 700; resizable: static[bool] = true; debug: static[bool] = not defined(release); callback: ExternalInvokeCb = nil): Webview {.inline.} =
+proc webView(title = ""; url = ""; width: Positive = 1000; height: Positive = 700; resizable: static[bool] = true;
+    debug: static[bool] = not defined(release); callback: ExternalInvokeCb = nil): Webview {.inline.} =
   result = cast[Webview](alloc0(sizeof(WebviewObj)))
   result.title = title
   result.url = url
@@ -322,9 +334,11 @@ proc webView(title = ""; url = ""; width: Positive = 1000; height: Positive = 70
   if callback != nil: result.externalInvokeCB = callback
   if result.init() != 0: return nil
 
-proc newWebView*(path: static[string] = ""; title = ""; width: Positive = 1000; height: Positive = 700; resizable: static[bool] = true; debug: static[bool] = not defined(release); callback: ExternalInvokeCb = nil,
-    skipTaskbar: static[bool] = false, windowBorders: static[bool] = true, focus: static[bool] = false, keepOnTop: static[bool] = false,
-    minimized: static[bool] = false, cssPath: static[string] = "", trayIcon: static[cstring] = "", fullscreen: static[bool] = false): Webview =
+proc newWebView*(path: static[string] = ""; title = ""; width: Positive = 1000; height: Positive = 700;
+    resizable: static[bool] = true; debug: static[bool] = not defined(release); callback: ExternalInvokeCb = nil;skipTaskbar: static[bool] = false; windowBorders: static[
+        bool] = true; focus: static[bool] = false; keepOnTop: static[bool] = false;
+    minimized: static[bool] = false; cssPath: static[string] = ""; trayIcon: static[cstring] = ""; fullscreen: static[
+        bool] = false): Webview =
   ## Create a new Window with given attributes, all arguments are optional.
   ## * `path` is the URL or Full Path to 1 HTML file, index of the Web GUI App.
   ## * `title` is the Title of the Window.
@@ -344,7 +358,7 @@ proc newWebView*(path: static[string] = ""; title = ""; width: Positive = 1000; 
   ## * CSS is embedded, if your app is used Offline, it will display Ok.
   ## * For templates that do CSS, remember that CSS must be injected *after DOM Ready*.
   ## * Is up to the developer to guarantee access to the HTML URL or File of the GUI.
- 
+
   result = webView(title, path, width, height, resizable, debug, callback)
   when skipTaskbar: result.setSkipTaskbar(skipTaskbar)
   when not windowBorders: result.setBorderlessWindow(windowBorders)
@@ -353,10 +367,11 @@ proc newWebView*(path: static[string] = ""; title = ""; width: Positive = 1000; 
   when minimized: webviewindow.setIconify(minimized)
   when trayIcon.len > 0: result.setTrayIcon(trayIcon, title.cstring, visible = true)
   when fullscreen: result.setFullscreen(fullscreen)
-  
+
   when path.endsWith".js": result.js(readFile(path))
   when path.endsWith".nim":
-    const compi = gorgeEx("nim js --out:" & path & ".js " & path & (when defined(release): " -d:release" else: "") & (when defined(danger): " -d:danger" else: ""))
+    const compi = gorgeEx("nim js --out:" & path & ".js " & path & (when defined(release): " -d:release" else: "") & (
+        when defined(danger): " -d:danger" else: ""))
     const jotaese = when compi.exitCode == 0: staticRead(path & ".js").strip.cstring else: "".cstring
     when not defined(release): echo jotaese
     when compi.exitCode == 0: echo result.js(jotaese)
